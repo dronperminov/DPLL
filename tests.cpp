5a103ab31e37463a29c1bc0fd3deee32f1d10cae
dronperminov <dronperminov@gmail.com>
2021-05-22 00:33:19 +0300

    add pigeon-hole data to test

--- tests.cpp
+++ tests.cpp
@@ -19,18 +19,21 @@ struct Task {
     bool isSat;
 };
 
-void TestOneFile(const string& path, bool isSat, DecisionStrategy strategy) {
+double TestOneFile(const string& path, bool isSat, DecisionStrategy strategy, int loops = 10) {
     TimePoint t0 = Time::now();
-    ifstream fin(path);
-    ConjunctiveNormalForm cnf(fin);
-    fin.close();
 
-    assert(cnf.DPLL(strategy) == isSat);
+    for (int loop = 0; loop < loops; loop++) {
+        ifstream fin(path);
+        ConjunctiveNormalForm cnf(fin, true, true);
+        fin.close();
+
+        assert(cnf.DPLL(strategy) == isSat);
+    }
 
     TimePoint t1 = Time::now();
     ms ellapsed = std::chrono::duration_cast<ms>(t1 - t0);
 
-    cout << "Time for test '" << path << "' (" + StrategyToString(strategy) + "): " << ellapsed.count() << " ms" << endl;
+    return (double)ellapsed.count() / loops;
 }
 
 double TestFromDir(Task task, DecisionStrategy strategy, int loops = 10) {
@@ -53,14 +56,14 @@ double TestFromDir(Task task, DecisionStrategy strategy, int loops = 10) {
 
 void PrintHeader(const vector<DecisionStrategy> &strategies) {
     cout << "## Performance of DPLL SAT solver" << endl;
-    cout << "|     cnf \\ strategy     |";
+    cout << "|         cnf \\ strategy         |";
 
     for (size_t i = 0; i < strategies.size(); i++)
         cout << setw(8) << StrategyToString(strategies[i]) << " |";
 
     cout << endl;
 
-    cout << "|           :-:          |";
+    cout << "|               :-:              |";
 
     for (size_t i = 0; i < strategies.size(); i++)
         cout << "     :-: |";
@@ -91,10 +94,18 @@ int main(int argc, char **argv) {
         { "data/unsat100/uuf100-0", 100, false },
     };
 
+    vector<Task> oneTasks = {
+        { "data/pigeon-hole/hole6.cnf", 1, false },
+        { "data/pigeon-hole/hole7.cnf", 1, false },
+        { "data/pigeon-hole/hole8.cnf", 1, false },
+        { "data/pigeon-hole/hole9.cnf", 1, false },
+        { "data/hanoi/hanoi4.cnf", 1, true },
+    };
+
     PrintHeader(strategies);
 
     for (size_t i = 0; i < tasks.size(); i++) {
-        cout << "| " << setw(22) << tasks[i].dirname << " |";
+        cout << "| " << setw(30) << tasks[i].dirname << " |";
 
         for (int j = 0; j < strategies.size(); j++)
             cout << " " << setw(7) << setprecision(3) << TestFromDir(tasks[i], strategies[j]) << " |";
@@ -102,9 +113,12 @@ int main(int argc, char **argv) {
         cout << endl;
     }
 
-    cout << endl;
+    for (size_t i = 0; i < oneTasks.size(); i++) {
+        cout << "| " << setw(30) << oneTasks[i].dirname << " |";
 
-    for (int i = 0; i < strategies.size(); i++) {
-        TestOneFile("data/hanoi/hanoi4.cnf", true, strategies[i]);
+        for (int j = 0; j < strategies.size(); j++)
+            cout << " " << setw(7) << setprecision(3) << TestOneFile(oneTasks[i].dirname, oneTasks[i].isSat, strategies[j]) << " |";
+
+        cout << endl;
     }
 }
